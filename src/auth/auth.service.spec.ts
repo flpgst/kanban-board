ae0c581c2f230118457afc64252730764e81f798
Filipe Augusto Gonçalves <flpgst@gmail.com>
2021-09-11 20:46:53 -0300

    Auth Service unit tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,79 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
 
+const user: User = {
+  id: 1,
+  username: 'admin',
+  password: '1234',
+};
+
+const resolvedJwt = {
+  access_token: 'jwtToken',
+};
+
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(user),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn().mockReturnValue('jwtToken'),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+    expect(usersService).toBeDefined();
+  });
+
+  describe('validateUser', () => {
+    // Arrange
+    const validatedUser = {
+      id: 1,
+      username: 'admin',
+    };
+
+    it('should return validated user', async () => {
+      expect(await authService.validateUser('admin', '1234')).toEqual(
+        validatedUser,
+      );
+    });
+
+    it('should return null if user not found', async () => {
+      const invalidUser = {
+        username: 'admin',
+        password: 'invalidPassword',
+      };
+      expect(
+        await authService.validateUser(
+          invalidUser.username,
+          invalidUser.password,
+        ),
+      ).toBeNull();
+    });
+
+    it('should return a JWT', async () => {
+      expect(await authService.login(user)).toEqual(resolvedJwt);
+    });
   });
 });
